899ec397a90f4efc6e0c41fd0b5ca742f8543222
Viacheslav S <grayraw13@outlook.com>
2024-05-11 19:17:55 +0300

    Changed theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ end)
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Solarized Dark - Patched'
+config.color_scheme = 'Website (Gogh)'
 
 config.window_background_opacity = 0.9
 config.launch_menu = {
