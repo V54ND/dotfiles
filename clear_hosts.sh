81b2ede69e284f88bff8c422670423f4b64be3b6
Viacheslav S <v8sand@gmail.com>
2025-08-28 21:36:11 +0300

    Removed host related sh files

--- clear_hosts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm /c/Windows/System32/drivers/etc/hosts
-echo "" >> /c/Windows/System32/drivers/etc/hosts
--- fill_hosts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mv /c/Users/vsandrikov/sxmproject/nissan-janus-cwp-portal/dev/etc-hosts-lines.win.txt /c/Windows/System32/drivers/etc/hosts
